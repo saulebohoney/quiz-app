b6c20508c6e86bebc5f85098e0e480c567bab765
beyonce knowles <beyonce@thinkful.com>
2017-05-15 16:47:36 -0400

    updated with template

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ const questions=[
     { 
         question:'this is questions three',
         answers:['a','b','c'],
-        correctAnswer: []
+        correctAnswer: ['b']
 
     },
     { 
         question:'this is questions four',
         answers:['a','b','c'],
-        correctAnswer: []
+        correctAnswer: ['b']
 
     },
     { 
         question:'this is questions five',
         answers:['a','b','c'],
-        correctAnswer: []
+        correctAnswer: ['b']
     }
 ];
 
@@ -36,7 +36,8 @@ const appState ={
     currentId: 0,
     currentQuestion: {},
     currentScore: 0,
-    userAnswer: ' '
+    userAnswer: ' ',
+    
 
     /*
     question:questions.question,
@@ -55,27 +56,56 @@ function addQuestion(state, index){
     //questions[x-1]
     //questions[state.currentId-1]
 
-    console.log(appState.currentId);
-    console.log(appState.currentQuestion);
+    //console.log(appState.currentId);
+   // console.log(appState.currentQuestion);
 }
 addQuestion();
 console.log(appState.currentId);
 
+//mod to check answers
 function checkAnswer(appState, userChoice) {
     appState.userAnswer = userChoice;
-    if (appState.userAnswer === appState.correctAnswer) {
+    if (userChoice == appState.currentQuestion.correctAnswer) {
       alert('correct!');
-    } else {alert(`Sorry! The correct answer is ${appState.correctAnswer}`)}
-    console.log(appState.userAnswer);
-    console.log(appState.correctAnswer);
+      appState.currentScore++;
+    } else {alert(`Sorry! The correct answer is ${appState.currentQuestion.correctAnswer}`)}
+    console.log(userChoice);
+    console.log(appState.currentQuestion.correctAnswer);
+    console.log(appState.currentScore);
 }
 checkAnswer(appState, 'b');
 
-//mod score
+console.log(appState.currentScore);
 
-//mod page number
+console.log(questions.length);
+//Question Template 
+const questionTemplate = function(appState){
+    return 
+    `<div>
+        <h1>Question ${appState.currentId}</h1>
+        <p>${appState.currentQuestion.question}</p>
 
-//
+        <form action="">    
+            <input type="radio" name="answer" value="a">${appState.currentQuestion.answers[0]}<br>
+            <input type="radio" name="answer" value="b">${appState.currentQuestion.answers[1]}<br>
+            <input type="radio" name="answer" value="c">${appState.currentQuestion.answers[2]}
+        </form>
+    </div>
+
+    <div>
+
+        <form>
+            <button>Submit</button>
+        </form>
+
+    <div>Current Question ${appState.currentId} out of ${questions.length}</div>
+    <div>Current Score: ${appState.currentScore} out of ${questions.length}</div>
+
+    </div>`;
+}
+questionTemplate(0);
+//console.log(questionTemplate);
+
 
 //render functions
 
