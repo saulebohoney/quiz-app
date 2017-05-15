71611dcf89820e02b49eace88bff0262d3b88011
Saule BN <beyonce@thinkful.com>
2017-05-15 15:44:39 -0400

    mod functions in progress

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const questions=[
     {
         question:'this is questions one',
         answers:['a','b','c'],
-        correctAnswer: []
+        correctAnswer: ['b']
     },
 
     {
         question:'this is questions two',
         answers:['a','b','c'],
-        correctAnswer: []
+        correctAnswer: ['b']
     },
     { 
         question:'this is questions three',
@@ -35,7 +35,8 @@ const questions=[
 const appState ={
     currentId: 0,
     currentQuestion: {},
-    currentScore: 0
+    currentScore: 0,
+    userAnswer: ' '
 
     /*
     question:questions.question,
@@ -49,14 +50,32 @@ const appState ={
 //mod functions
 function addQuestion(state, index){
     appState.currentId++;
-    appState.currentQuestion = questions[appState.currentId-1]
+    appState.currentQuestion = questions[appState.currentId-1];
     //questions[2]
     //questions[x-1]
     //questions[state.currentId-1]
 
     console.log(appState.currentId);
+    console.log(appState.currentQuestion);
 }
+addQuestion();
 console.log(appState.currentId);
+
+function checkAnswer(appState, userChoice) {
+    appState.userAnswer = userChoice;
+    if (appState.userAnswer === appState.correctAnswer) {
+      alert('correct!');
+    } else {alert(`Sorry! The correct answer is ${appState.correctAnswer}`)}
+    console.log(appState.userAnswer);
+    console.log(appState.correctAnswer);
+}
+checkAnswer(appState, 'b');
+
+//mod score
+
+//mod page number
+
+//
 
 //render functions
 
